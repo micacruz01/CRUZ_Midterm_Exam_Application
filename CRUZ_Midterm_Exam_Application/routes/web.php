3c7aa1d9e488550a80e3aaf91d7640e54281d18e
micacruz01 <micac0963@gmail.com>
2026-02-20 01:58:46 +0000

    modified final

--- CRUZ_Midterm_Exam_Application/routes/web.php
+++ CRUZ_Midterm_Exam_Application/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/books', [ProductController::class, 'index']);
+
+Route::get('/', [ProductController::class, 'index']);
